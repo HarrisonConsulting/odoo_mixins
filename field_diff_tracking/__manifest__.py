f452769062500d2e77f2e2300c30b93ff01fb83e
Matt Harrison <matt@harrison.consulting>
2025-03-10 07:05:46 -0400

    Bump version to 17.0.1.0.1 and update author name in manifest

--- field_diff_tracking/__manifest__.py
+++ field_diff_tracking/__manifest__.py
@@ -1,11 +1,11 @@
 {
     "name": "Field Diff Tracking",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "summary": "Track diffs for text, char, and HTML fields",
     "sequence": 0,
     "description": """The Field Diff Tracking module provides a mixin for Odoo models to track and display differences in text, char, and HTML fields. This module enhances Odoo's native change tracking by providing a more detailed and visually appealing diff view in the chatter.""",
     "category": "Technical",
-    "author": "Harrison Consulting, LLC",
+    "author": "Harrison Consulting",
     "website": "https://www.harrison.consulting",
     "license": "LGPL-3",
     "depends": ["base", "mail"],
